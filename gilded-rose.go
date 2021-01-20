a651c4309499ff04fbb82314b1567fe24f8df67b
James Huynh <jameshdk@spdigital.sg>
2021-01-20 09:35:02 +0800

    Update solution

--- gilded-rose.go
+++ gilded-rose.go
@@ -9,50 +9,79 @@ type Item struct {
 // UpdateQuality updates quality of input items
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
-			}
-		}
+		updateQualityForItem(items[i])
+	}
+}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
+func updateQualityForItem(item *Item) {
+	switch item.name {
+	case "Sulfuras, Hand of Ragnaros":
+		updateSufurasItem(item)
+	case "Aged Brie":
+		updateAgedBrieItem(item)
+	case "Backstage passes to a TAFKAL80ETC concert":
+		updateBackstageItem(item)
+	case "Conjured":
+		updateConjuredItem(item)
+	default:
+		updateOtherItem(item)
+	}
+}
 
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
-			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
-			}
-		}
+// Do nothing with Sulfuras item
+func updateSufurasItem(item *Item) {
+	// nothing here
+}
+
+// Do nothing with Sulfuras item
+func updateAgedBrieItem(item *Item) {
+	item.sellIn = item.sellIn - 1
+	item.quality = item.quality + 1
+	if item.quality > 50 {
+		item.quality = 50
+	}
+}
+
+// Do nothing with Sulfuras item
+func updateBackstageItem(item *Item) {
+	if item.sellIn <= 0 {
+		item.quality = 0
+	} else if item.sellIn <= 5 {
+		item.quality = item.quality + 3
+	} else if item.sellIn <= 10 {
+		item.quality = item.quality + 2
+	} else {
+		item.quality = item.quality + 1
+	}
+
+	item.sellIn = item.sellIn - 1
+	if item.quality > 50 {
+		item.quality = 50
+	}
+}
+
+func updateOtherItem(item *Item) {
+	if item.quality > 0 {
+		item.quality = item.quality - 1
+	}
+
+	item.sellIn = item.sellIn - 1
+	if item.sellIn < 0 && item.quality > 0 {
+		item.quality = item.quality - 1
+	}
+}
+
+func updateConjuredItem(item *Item) {
+	if item.quality > 0 {
+		item.quality = item.quality - 2
+	}
+
+	item.sellIn = item.sellIn - 1
+	if item.sellIn < 0 && item.quality > 0 {
+		item.quality = item.quality - 2
+	}
+
+	if item.quality < 0 {
+		item.quality = 0
 	}
 }
--- gilded-rose_test.go
+++ gilded-rose_test.go
@@ -1,21 +1,327 @@
 package main
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Foo(t *testing.T) {
-	var items = []*Item{
-		{"foo", 0, 0},
+// - All items have a Quality value which denotes how valuable the item is
+// - At the end of each day our system lowers both values for every item
+// - Once the sell by date has passed, Quality degrades twice as fast
+// - The Quality of an item is never negative
+func TestOtherItems(t *testing.T) {
+	testcases := []struct {
+		items           []*Item
+		expectedSellIn  int
+		expectedQuality int
+	}{
+		{
+			items: []*Item{
+				{
+					"Other",
+					5,
+					20,
+				},
+			},
+			expectedSellIn:  4,
+			expectedQuality: 19,
+		},
+		{
+			items: []*Item{
+				{
+					"Other",
+					0,
+					20,
+				},
+			},
+			expectedSellIn:  -1,
+			expectedQuality: 18,
+		},
+		{
+			items: []*Item{
+				{
+					"Other",
+					-1,
+					1,
+				},
+			},
+			expectedSellIn:  -2,
+			expectedQuality: 0,
+		},
+		{
+			items: []*Item{
+				{
+					"Other",
+					-1,
+					10,
+				},
+			},
+			expectedSellIn:  -2,
+			expectedQuality: 8,
+		},
 	}
 
-	UpdateQuality(items)
+	for i, tc := range testcases {
+		tc := tc
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
 
-	// if items[0].name != "fixme" {
-	// 	t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
-	// }
+			UpdateQuality(tc.items)
+			require.Equal(t, tc.expectedQuality, tc.items[0].quality)
+			require.Equal(t, tc.expectedSellIn, tc.items[0].sellIn)
+		})
+	}
+}
 
-	require.Equal(t, true, true)
+func TestConjuredItems(t *testing.T) {
+	testcases := []struct {
+		items           []*Item
+		expectedSellIn  int
+		expectedQuality int
+	}{
+		{
+			items: []*Item{
+				{
+					"Conjured",
+					5,
+					20,
+				},
+			},
+			expectedSellIn:  4,
+			expectedQuality: 18,
+		},
+		{
+			items: []*Item{
+				{
+					"Conjured",
+					0,
+					20,
+				},
+			},
+			expectedSellIn:  -1,
+			expectedQuality: 16,
+		},
+		{
+			items: []*Item{
+				{
+					"Conjured",
+					-1,
+					1,
+				},
+			},
+			expectedSellIn:  -2,
+			expectedQuality: 0,
+		},
+		{
+			items: []*Item{
+				{
+					"Conjured",
+					-1,
+					10,
+				},
+			},
+			expectedSellIn:  -2,
+			expectedQuality: 6,
+		},
+	}
+
+	for i, tc := range testcases {
+		tc := tc
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+
+			UpdateQuality(tc.items)
+			require.Equal(t, tc.expectedQuality, tc.items[0].quality)
+			require.Equal(t, tc.expectedSellIn, tc.items[0].sellIn)
+		})
+	}
+}
+
+// "Aged Brie" actually increases in Quality the older it gets
+// The Quality of an item is never more than 50
+func TestAgedBrie(t *testing.T) {
+	testcases := []struct {
+		items           []*Item
+		expectedSellIn  int
+		expectedQuality int
+	}{
+		{
+			items: []*Item{
+				{
+					"Aged Brie",
+					5,
+					20,
+				},
+			},
+			expectedSellIn:  4,
+			expectedQuality: 21,
+		},
+		{
+			items: []*Item{
+				{
+					"Aged Brie",
+					10,
+					20,
+				},
+			},
+			expectedSellIn:  9,
+			expectedQuality: 21,
+		},
+		{
+			items: []*Item{
+				{
+					"Aged Brie",
+					20,
+					20,
+				},
+			},
+			expectedSellIn:  19,
+			expectedQuality: 21,
+		},
+		{
+			items: []*Item{
+				{
+					"Aged Brie",
+					20,
+					50,
+				},
+			},
+			expectedSellIn:  19,
+			expectedQuality: 50,
+		},
+	}
+
+	for i, tc := range testcases {
+		tc := tc
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+
+			UpdateQuality(tc.items)
+			require.Equal(t, tc.expectedQuality, tc.items[0].quality)
+			require.Equal(t, tc.expectedSellIn, tc.items[0].sellIn)
+		})
+	}
+}
+
+// "Sulfuras"
+// is a legendary item and as such its Quality is 80 and it never alters.
+func TestSulfuras(t *testing.T) {
+	testcases := []struct {
+		items           []*Item
+		expectedSellIn  int
+		expectedQuality int
+	}{
+		{
+			items: []*Item{
+				{
+					"Sulfuras, Hand of Ragnaros",
+					5,
+					80,
+				},
+			},
+			expectedSellIn:  5,
+			expectedQuality: 80,
+		},
+		{
+			items: []*Item{
+				{
+					"Sulfuras, Hand of Ragnaros",
+					10,
+					80,
+				},
+			},
+			expectedSellIn:  10,
+			expectedQuality: 80,
+		},
+		{
+			items: []*Item{
+				{
+					"Sulfuras, Hand of Ragnaros",
+					20,
+					80,
+				},
+			},
+			expectedSellIn:  20,
+			expectedQuality: 80,
+		},
+	}
+
+	for i, tc := range testcases {
+		tc := tc
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+
+			UpdateQuality(tc.items)
+			require.Equal(t, tc.expectedQuality, tc.items[0].quality)
+			require.Equal(t, tc.expectedSellIn, tc.items[0].sellIn)
+		})
+	}
+}
+
+// "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+//	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+//	Quality drops to 0 after the concert
+func TestBackStagePasses(t *testing.T) {
+	testcases := []struct {
+		items           []*Item
+		expectedSellIn  int
+		expectedQuality int
+	}{
+		{
+			items: []*Item{
+				{
+					"Backstage passes to a TAFKAL80ETC concert",
+					5,
+					20,
+				},
+			},
+			expectedSellIn:  4,
+			expectedQuality: 23,
+		},
+		{
+			items: []*Item{
+				{
+					"Backstage passes to a TAFKAL80ETC concert",
+					10,
+					20,
+				},
+			},
+			expectedSellIn:  9,
+			expectedQuality: 22,
+		},
+		{
+			items: []*Item{
+				{
+					"Backstage passes to a TAFKAL80ETC concert",
+					20,
+					20,
+				},
+			},
+			expectedSellIn:  19,
+			expectedQuality: 21,
+		},
+		{
+			items: []*Item{
+				{
+					"Backstage passes to a TAFKAL80ETC concert",
+					0,
+					20,
+				},
+			},
+			expectedSellIn:  -1,
+			expectedQuality: 0,
+		},
+	}
+
+	for i, tc := range testcases {
+		tc := tc
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+
+			UpdateQuality(tc.items)
+			require.Equal(t, tc.expectedQuality, tc.items[0].quality)
+			require.Equal(t, tc.expectedSellIn, tc.items[0].sellIn)
+		})
+	}
 }
